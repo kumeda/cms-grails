53bce124650699ada87eadd77a9ec324be4a861e
kazuhiko.umeda <kumeda1@gmail.com>
2015-10-16 16:23:02 -0700

    update domain_class

--- grails-app/domain/com/chefkoochooloo/dao/Flag.groovy
+++ grails-app/domain/com/chefkoochooloo/dao/Flag.groovy
@@ -1,7 +1,34 @@
 package com.chefkoochooloo.dao
 
-class Flag {
+import groovy.transform.ToString
 
-    static constraints = {
-    }
+@ToString(includePackage=false, includeNames=true, excludes="")
+class Flag {
+    
+  Integer id
+  Integer ent
+  Integer opt
+  String name 
+
+  static mapping = {
+    table '`flag`'
+    cache true
+    version false
+  }
+
+  static constraints = {
+    ent blank: true
+    opt blank: true
+  }
 }
+
+/*
+CREATE TABLE ZFLAG ( 
+Z_PK INTEGER PRIMARY KEY, 
+Z_ENT INTEGER, 
+Z_OPT INTEGER, 
+ZNAME VARCHAR );
+
+
+CREATE TABLE ZFLAG ( Z_PK INTEGER PRIMARY KEY, Z_ENT INTEGER, Z_OPT INTEGER, ZNAME VARCHAR );
+*/
--- grails-app/domain/com/chefkoochooloo/dao/RecipeType.groovy
+++ grails-app/domain/com/chefkoochooloo/dao/RecipeType.groovy
@@ -1,7 +1,25 @@
 package com.chefkoochooloo.dao
 
+import groovy.transform.ToString
+
+@ToString(includePackage=false, includeNames=true, excludes="")
 class RecipeType {
 
-    static constraints = {
-    }
+  Integer id
+  Integer ent
+  Integer opt
+  String name 
+  String presentation
+  String time
+
+  static mapping = {
+    table '`recipe`'
+    cache true
+    version false
+  }
+
+  static constraints = {
+    ent blank: true
+    opt blank: true
+  }
 }
