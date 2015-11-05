d0f0d8e13d62576f75450fca564289c04327cc91
ikakara <allen.arakaki@gmail.com>
2015-11-05 09:58:07 -0800

    constants example

---
+++ src/groovy/com/chefkoochooloo/app/ChefKConstants.groovy
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.chefkoochooloo.app
+
+/**
+ *
+ * @author Allen
+ */
+class ChefKConstants {
+  
+  static final int TYPE_ZERO = 0
+  static final int TYPE_ONE = 1
+  
+  
+  // ChefKConstants.TYPE_ZERO
+	
+}
+
